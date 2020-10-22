c4b70169dbf00b9f93d1bfaf3245af0a274a414a
Alazar-dev <mealazartadesse@gmail.com>
2020-10-22 11:26:31 +0300

    on sale computed

--- main.js
+++ main.js
@@ -4,17 +4,30 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            selectedVariant: 0,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            selectedVariant: 0, 
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            // solution
-            onSale: true
-            // solution
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+            ]
         }
+        
     },
+    computed: {
+            title(){
+                return this.brand + ' ' + this.product
+            },
+            image(){
+                return this.variants[this.selectedVariant].image
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].quantity
+            },
+            sale(){
+                return this.brand + ' ' + this.product + ' is ON SALE!!'
+            }
+        },
     methods: {
         addToCart() {
             this.cart += 1
@@ -22,24 +35,5 @@ const app = Vue.createApp({
         updateVariant(index) {
             this.selectedVariant = index
         }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        // solution
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
-            }
-            return ''
-        }
-        // solution
     }
 })
